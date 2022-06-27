4497c02f1111fafb6e510c2eb3f9aaf63d60a5f7
Izgoi1 <vadim.sichek@gmail.com>
2022-06-27 15:09:46 +0300

    Exercises

---
+++ Uprajnenie89.py
@@ -0,0 +1,29 @@
+#Функция которая переводит числа в числительные в диапазоне от 1 до 12.
+def Numerals(Number):
+    if Number == 1:
+        print('First')
+    elif Number == 2:
+        print('Second')
+    elif Number == 3:
+        print('Third')
+    elif Number == 4:
+        print('Fourth')
+    elif Number == 5:
+        print('Fifth')
+    elif Number == 6:
+        print('Sixth')
+    elif Number == 7:
+        print('Seventh')
+    elif Number == 8:
+        print('Eight')
+    elif Number == 9:
+        print('Ninth')
+    elif Number == 10:
+        print('Tenth')
+    elif Number == 11:
+        print('Eleventh')
+    elif Number == 12:
+        print('Twelfth')
+    else:
+        print('')
+
